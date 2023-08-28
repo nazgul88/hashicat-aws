e70df80e2289d370961d718ef48c8df3160d1c00
nazka <nazka@amazon.com>
2023-08-28 21:15:37 +0000

    Added s3 bucket module

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "tf-demo-amzn"
     workspaces {
       name = "hashicat-aws"
